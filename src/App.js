7f008037f131eae4939730482856139f25264a86
Ibrahim Ilhan <34579924+ibrahimilhan35@users.noreply.github.com>
2023-12-11 15:17:51 +0300

    Pure and Higher-Order Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import ColorButtonA from "./ColorButtonA";
+import ColorButtonB from "./ColorButtonB";
 
 function App() {
   return (
     <div className="App">
-      <hi1>Add your code here!</hi1>
+      <section className="container">
+        <h1>Pure and Higher-Order Components</h1>
+        <ColorButtonA />
+        <ColorButtonB />
+      </section>
     </div>
   );
 }
